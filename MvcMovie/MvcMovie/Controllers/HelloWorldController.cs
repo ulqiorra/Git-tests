742ae65ffbd1c7b0c91e44550024525d1cdc4062
Alexander.Lapko <storm-antiplayer@mail.ru>
2013-10-20 18:48:19 +0300

    Trying to add MVC app

---
+++ MvcMovie/MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //
+        // GET: /HelloWorld/
+
+        public ActionResult Index()//string Index()
+        {
+            return View();//"This is my <b>default</b> action...";
+        }
+
+        //
+        //GET: /HelloWorld/Welcome
+
+        public ActionResult Welcome(string name, int numTimes = 1)//string Welcome(string name, int numTimes = 1)
+        {
+            ViewBag.Message = "Hello " + name;
+            ViewBag.NumTimes = numTimes;
+
+            return View();
+            //return HttpUtility.HtmlEncode("Hello "+ name + ", NumTimes is: " + numTimes);//"This is the Welcome action method...";
+        }
+
+    }
+}
---
+++ MvcMovie/MvcMovie/Scripts/globalize.culture.en-US.js
@@ -0,0 +1,20 @@
+(function( window, undefined ) {
+
+var Globalize;
+
+if ( typeof require !== "undefined" &&
+        typeof exports !== "undefined" &&
+        typeof module !== "undefined" ) {
+        // Assume CommonJS
+        Globalize = require( "globalize" );
+} else {
+        // Global variable
+        Globalize = window.Globalize;
+}
+
+Globalize.addCultureInfo( "en-US", "default", {
+        name: "en-US",
+        englishName: "English (United States)"
+});
+
+}( this ));
